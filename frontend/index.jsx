dcf74511ebbbea61c19f0c8620df03ebb9215e1c
Eugene Cheng <ugene1213@gmail.com>
2017-04-16 23:11:03 -0400

    remove tests

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/root';
-import { REQUEST_BUSINESSES, requestBusinesses, requestBusiness, createReview } from './actions/business_actions'; //TESTING
-import { fetchBusinesses, fetchBusiness } from './util/business_api_util'; //TESTING
-import { toArray } from './reducers/selectors';
-
 
 document.addEventListener('DOMContentLoaded', () => {
     let store;
@@ -19,13 +15,6 @@ document.addEventListener('DOMContentLoaded', () => {
     } else {
       store = configureStore();
     }
-    window.store = store;
-    window.toArray = toArray;
-    window.createReview = createReview;
-    window.requestBusinesses = requestBusinesses;
-    window.fetchBusinesses = fetchBusinesses;
-    window.requestBusiness = requestBusiness;
-    window.fetchBusiness = fetchBusiness;
     const root = document.getElementById('root');
     ReactDOM.render(<Root store={store}/>, root);
 });
